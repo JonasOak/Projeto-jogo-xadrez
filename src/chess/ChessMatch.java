e407e90bd4a12594262f0a450877ab90ec4dd42c
Jonas Carvalho <jonasgabrielsc@gmail.com>
2023-05-09 16:43:25 -0300

    Layout do xadrez e print do tabuleiro

---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,22 @@
+package chess;
+
+import boardgame.Board;
+
+public class ChessMatch {
+
+	private Board board;
+	
+	public ChessMatch() {
+		board = new Board(8, 8);
+	}
+	
+	public ChessPiece[][] getPieces() {
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for (byte i=0; i<board.getRows(); i++) {
+			for (byte j=0; i<board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
+			}
+		}
+		return mat;
+	}
+}
